0088679933a5d2068ba8c112002805e2190afde7
Olivio Neto <netoolivio34@gmail.com>
2024-07-18 11:25:06 -0300

    feat: adicionando a categoria de itens e a lista de componentes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './_components/header'
 import Search from './_components/search'
+import CategoryList from './_components/category-list'
 
 const Home = () => {
   return (
@@ -8,6 +9,10 @@ const Home = () => {
       <Header />
       <div className="px-5 pt-6">
         <Search />
+      </div>
+
+      <div className="px-5 pt-6">
+      <CategoryList/>
       </div>
     </>
   )
