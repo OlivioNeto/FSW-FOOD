9066a2165652818f282aad03894b43b681633885
Olivio Neto <netoolivio34@gmail.com>
2024-07-19 09:30:51 -0300

    feat: adicionando o proom banner na home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './_components/header'
 import Search from './_components/search'
 import CategoryList from './_components/category-list'
+import Image from 'next/image'
 
 const Home = () => {
   return (
@@ -12,7 +13,18 @@ const Home = () => {
       </div>
 
       <div className="px-5 pt-6">
-      <CategoryList/>
+        <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promobanner.png"
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vm"
+        />
       </div>
     </>
   )
