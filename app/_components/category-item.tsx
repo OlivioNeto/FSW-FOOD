042a28ab538252e3b87ef453f0c19f49b8a6c00d
Olivio Neto <netoolivio34@gmail.com>
2024-07-29 09:10:48 -0300

    style: mudando o estilo do category-item

--- app/_components/category-item.tsx
+++ app/_components/category-item.tsx
@@ -7,7 +7,7 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
     return (
-        <div className="flex items-center gap-3 rounded-full py-3 px-4 shadow-md">
+        <div className="flex justify-center items-center gap-3 rounded-full py-3 px-4 shadow-md">
             <Image
                 src={category.imageUrl}
                 alt={category.name}
